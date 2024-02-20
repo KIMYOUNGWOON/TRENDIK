bbf04761dcda742374a2ca4e59a6c5d3e7e9463e
KIMYOUNGWOON <youngwn2130@gmail.com>
2024-02-20 16:07:13 +0900

    에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // 빌드 후 자동으로 번들 분석 보고서를 열지 여부
+      filename: "./dist/stats.html", // 분석 보고서 파일 경로 및 이름
+    }),
+  ],
+});
